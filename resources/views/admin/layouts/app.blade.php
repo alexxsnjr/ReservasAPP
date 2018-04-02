b188bb2df05cafbbb269a8ddfbbfc5b9e966d83e
Alex Navarro <byalexxfg@gmail.com>
2018-04-02 20:43:46 +0200

    Ignore Storage

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -65,7 +65,7 @@ desired effect
         <!-- Logo -->
         <a href="#" class="logo">
             <!-- mini logo for sidebar mini 50x50 pixels -->
-            <span class="logo-mini"><b>AN</b></span>
+            <span class="logo-mini"><b>GDA</b></span>
             <!-- logo for regular state and mobile devices -->
             <span class="logo-lg"><b>{{ config('app.name') }}</b></span>
         </a>
