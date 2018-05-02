6974f5e09bf31551f91e0f34caca04e78ec4637f
Alex Navarro <byalexxfg@gmail.com>
2018-05-02 20:29:26 +0200

    Consulta filtrar aulas

--- app/Http/Controllers/Api/ReservaController.php
+++ app/Http/Controllers/Api/ReservaController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Api;
 
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\DB;
@@ -17,16 +18,13 @@ class ReservaController extends Controller
             'turno'=>'required',
             'hora'=>  'required',
             'tipo'=>'required',
-
-
         ]);
 
-        $aulas = DB::table('aulas')
-            ->where('aulas.tipo', '=', $request->tipo)
-            ->where('aulas.aforo', '>=', $request->aforo)
-            ->get();
+        $fecha= Carbon::parse($request->fecha)->format('Y-m-d');
+        //dd($fecha);
 
-        //dd($aulas);
+        $aulas = DB::select('SELECT aulas.id as ID, aulas.nombre as nombre from aulas, equipamientos where aulas.id = equipamientos.aula_id and aulas.tipo = "'.$request->tipo.'" and aulas.aforo >= "'.$request->aforo.'" and equipamientos.nombre = "'.$request->requerimientos[0].'" and aulas.id not in (SELECT aula_id FROM reservas WHERE fecha = "'.$fecha.'" AND turno = "'.$request->turno.'" AND hora = "'.$request->hora.'")');
+
         return response()->json(compact('aulas'));
     }
 }
