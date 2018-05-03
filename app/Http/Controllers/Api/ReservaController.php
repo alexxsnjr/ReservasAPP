e876cea79d4471c6c1663a4ca69fef20af7699c9
Alex Navarro <byalexxfg@gmail.com>
2018-05-03 20:44:56 +0200

    Arreglo comprobarDisponibilidad $fecha

--- app/Http/Controllers/Api/ReservaController.php
+++ app/Http/Controllers/Api/ReservaController.php
@@ -9,6 +9,7 @@ use Illuminate\Support\Facades\DB;
 
 class ReservaController extends Controller
 {
+
     public function comprobarDisponibilidad(Request $request)
     {
         $this->validate($request, [
@@ -16,15 +17,16 @@ class ReservaController extends Controller
             'fecha'=>'required',
             'aforo'=>'required',
             'turno'=>'required',
-            'hora'=>  'required',
+            'hora'=>  'required|integer',
             'tipo'=>'required',
         ]);
 
-        $fecha= Carbon::parse($request->fecha)->format('Y-m-d');
+        $fecha= Carbon::parse($request->fecha)->addDay()->format('Y-m-d');
         //dd($fecha);
 
         $aulas = DB::select('SELECT aulas.id as ID, aulas.nombre as nombre from aulas, equipamientos where aulas.id = equipamientos.aula_id and aulas.tipo = "'.$request->tipo.'" and aulas.aforo >= "'.$request->aforo.'" and equipamientos.nombre = "'.$request->requerimientos[0].'" and aulas.id not in (SELECT aula_id FROM reservas WHERE fecha = "'.$fecha.'" AND turno = "'.$request->turno.'" AND hora = "'.$request->hora.'")');
 
         return response()->json(compact('aulas'));
     }
+
 }
